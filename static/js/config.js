246b98665b54c80e08d249e42410b7e0ffd2d380
Cody Morris <codyc.morris@gmail.com>
2021-10-02 13:42:12 -0400

    working markers

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,3 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiY29keS1tIiwiYSI6ImNrdTdleXZiZjIzYTEybm5paGt4bGxxMmUifQ.1epC0Xk8m70APv0heMk2tQ";
+const API_KEY2 = 'pk.eyJ1IjoiY29keS1tIiwiYSI6ImNrdTl6OTRyOTA5aWYyc3A0ejlybnczNXYifQ.HXdJnHrYFtI1E32l-2lIxQ'
